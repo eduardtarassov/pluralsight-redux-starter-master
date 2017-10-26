d9aa7db90890668c2fc6bf6c3ac2e8758c215a11
Eduard <eduardtarassov@gmail.com>
2017-10-27 00:30:52 +0100

    Initial App Structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill'; // babel transcribes ES6 to ES5. But polyfill is required for features which babel can't transcribe (object.assign).
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; // Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+	<Router history={browserHistory} routes={routes} />,
+	document.getElementById('app')
+);
