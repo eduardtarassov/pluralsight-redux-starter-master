d2aee9fb1e56d5828b6f9cf61aaa0ebacf1edd04
Eduard <eduardtarassov@gmail.com>
2017-11-09 22:50:19 +0000

    Async Writes in Redux

---
+++ src/components/common/TextInput.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+const TextInput = (props) => {
+    return (
+
+    );
+}
+
+TextInput.propTypes = {
+    myProp: PropTypes.string.isRequired
+};
+
+export default TextInput;
---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,34 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+class ManageCoursePage extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+        );
+    }
+}
+
+ManageCoursePage.propTypes = {
+    // myProp: PropTypes.string.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        courses: state
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    };
+}
+
+// connect function returns a function which is called with CoursesPage param
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
